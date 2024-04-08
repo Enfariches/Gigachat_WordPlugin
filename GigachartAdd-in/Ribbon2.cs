230194f1fd76fa0d84f25aa60cd4b56a12d735a1
Enfariches <serg-eburg2@yandex.ru>
2024-04-08 15:25:54 +0500

    VSTO

---
+++ GigachartAdd-in/Ribbon2.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+using Office = Microsoft.Office.Core;
+using System.Drawing;
+
+// TODO:  Выполните эти шаги, чтобы активировать элемент XML ленты:
+
+// 1: Скопируйте следующий блок кода в класс ThisAddin, ThisWorkbook или ThisDocument.
+
+//  protected override Microsoft.Office.Core.IRibbonExtensibility CreateRibbonExtensibilityObject()
+//  {
+//      return new Ribbon2();
+//  }
+
+// 2. Создайте методы обратного вызова в области "Обратные вызовы ленты" этого класса, чтобы обрабатывать действия
+//    пользователя, например нажатие кнопки. Примечание: если эта лента экспортирована из конструктора ленты,
+//    переместите свой код из обработчиков событий в методы обратного вызова и модифицируйте этот код, чтобы работать с
+//    моделью программирования расширения ленты (RibbonX).
+
+// 3. Назначьте атрибуты тегам элементов управления в XML-файле ленты, чтобы идентифицировать соответствующие методы обратного вызова в своем коде.  
+
+// Дополнительные сведения можно найти в XML-документации для ленты в справке набора средств Visual Studio для Office.
+
+
+namespace GigachartAdd_in
+{
+    [ComVisible(true)]
+    public class Ribbon2 : Office.IRibbonExtensibility
+    {
+        private Office.IRibbonUI ribbon;
+
+        public Ribbon2()
+        {
+        }
+
+        public void GetButtonID(Office.IRibbonControl control)
+        {
+            Form1 form = new Form1();
+            form.Show();
+            Microsoft.Office.Interop.Word.Range currentRange = Globals.ThisAddIn.Application.Selection.Range;
+            currentRange.Text = "Текст создан контекстным меню";
+        }
+
+        #region Элементы IRibbonExtensibility
+
+        public string GetCustomUI(string ribbonID)
+        {
+            return GetResourceText("GigachartAdd_in.Ribbon2.xml");
+        }
+
+        #endregion
+
+        #region Обратные вызовы ленты
+        //Информацию о методах создания обратного вызова см. здесь. Дополнительные сведения о методах добавления обратного вызова см. по ссылке https://go.microsoft.com/fwlink/?LinkID=271226
+
+        public void Ribbon_Load(Office.IRibbonUI ribbonUI)
+        {
+            this.ribbon = ribbonUI;
+        }
+
+        #endregion
+
+        #region Вспомогательные методы
+
+        private static string GetResourceText(string resourceName)
+        {
+            Assembly asm = Assembly.GetExecutingAssembly();
+            string[] resourceNames = asm.GetManifestResourceNames();
+            for (int i = 0; i < resourceNames.Length; ++i)
+            {
+                if (string.Compare(resourceName, resourceNames[i], StringComparison.OrdinalIgnoreCase) == 0)
+                {
+                    using (StreamReader resourceReader = new StreamReader(asm.GetManifestResourceStream(resourceNames[i])))
+                    {
+                        if (resourceReader != null)
+                        {
+                            return resourceReader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+        #endregion
+    }
+}
---
+++ GigachartAdd-in/ThisAddIn.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Word = Microsoft.Office.Interop.Word;
+using Office = Microsoft.Office.Core;
+using Microsoft.Office.Tools.Word;
+using Microsoft.Office.Core;
+using System.Windows.Forms;
+
+namespace GigachartAdd_in
+{
+    public partial class ThisAddIn
+    {
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+
+        }
+
+        protected override Microsoft.Office.Core.IRibbonExtensibility CreateRibbonExtensibilityObject()
+        {
+            return new Ribbon2();
+        }
+
+        #region Код, автоматически созданный VSTO
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+        
+        #endregion
+    }
+}
